727b7068140f5f59a4a6fc0f4016cea7a7144a82
cxmeel <21029087+cxmeel@users.noreply.github.com>
2024-11-24 19:18:09 +0000

    Remove YAML plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 
 import { sveltekit } from "@sveltejs/kit/vite";
-import yaml from "unplugin-yaml/vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), yaml()],
+	plugins: [sveltekit()],
 });
